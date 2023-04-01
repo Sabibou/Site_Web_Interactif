12d759127c64caae38fb5fa4fc57600d637103c8
Rodrigo <Rodrigo.FERREIRA_RODRIGUES@etu.uca.fr>
2023-04-01 17:49:02 +0200

    debut systeme de chatbox (html, css, js)

--- serv/www/js/chatbox.js
+++ serv/www/js/chatbox.js
@@ -1,9 +1,7 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const userpwd = document.getElementById('userpwd');
-const response = document.getElementById("response");
-const register = document.getElementById('register');
-const submit = document.getElementsByTagName("input")[2];
+const msg = document.getElementById('msg');
+const submit = document.getElementById("send");
+const chat = document.getElementById('chat');
 // input[2].addEventListener('click', submitForm, false);
 
 submit.addEventListener("click", sendForm, false);
@@ -12,7 +10,7 @@ function sendForm(event) {
     event.preventDefault();
     const xhr = new XMLHttpRequest();
     const method = "POST";
-    const url = "../htbin/login.py";
+    const url = "../htbin/chatsend.py";
     
     xhr.open(method, url, true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -23,19 +21,21 @@ function sendForm(event) {
         if (status === 0 || (status >= 200 && status < 400)) {
           // The request has been completed successfully
           console.log(xhr.responseText);
-          response.innerHTML = xhr.responseText;
+          var p = document.createElement("p");
+          p.innerHTML = xhr.responseText;
+          chat.append(p); 
           if(xhr.responseText.indexOf("Bonjour") != -1){
             form.remove();
-            register.remove();
-            response.classList.add('success');
+            //register.remove();
+            //response.classList.add('success');
           }
           else{
-            response.classList.add('error');
+            //response.classList.add('error');
           }
         } else {
           // Oh no! There has been an error with the request!
         }
       }
     };
-    xhr.send("username="+encodeURIComponent(username.value)+ "&userpwd=" + encodeURIComponent(userpwd.value)); 
+    xhr.send("msg="+encodeURIComponent(msg.value)); 
 }

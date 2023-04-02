f7d0bd5cfcc9f61e0bc56ee5cdbd68b3f146a7a2
Rodrigo <Rodrigo.FERREIRA_RODRIGUES@etu.uca.fr>
2023-04-02 15:52:50 +0200

    finalisation fonctionnalite de la chatbox

--- serv/www/js/chatbox.js
+++ serv/www/js/chatbox.js
@@ -2,6 +2,7 @@ const form = document.getElementById('form');
 const msg = document.getElementById('msg');
 const submit = document.getElementById("send");
 const chat = document.getElementById('chat');
+const errorMsg = document.getElementById('error-msg');
 // input[2].addEventListener('click', submitForm, false);
 
 submit.addEventListener("click", sendForm, false);
@@ -20,17 +21,18 @@ function sendForm(event) {
         const status = xhr.status;
         if (status === 0 || (status >= 200 && status < 400)) {
           // The request has been completed successfully
-          console.log(xhr.responseText);
-          var p = document.createElement("p");
-          p.innerHTML = xhr.responseText;
-          chat.append(p); 
-          if(xhr.responseText.indexOf("Bonjour") != -1){
-            form.remove();
-            //register.remove();
-            //response.classList.add('success');
+          let obj = JSON.parse(xhr.responseText);
+
+          console.log(obj.msg);
+
+          if(obj.num != 0){
+
+            msg.classList.add('error');
+            errorMsg.innerHTML = obj.msg;
           }
           else{
-            //response.classList.add('error');
+
+            getMsg(false);
           }
         } else {
           // Oh no! There has been an error with the request!
@@ -39,3 +41,65 @@ function sendForm(event) {
     };
     xhr.send("msg="+encodeURIComponent(msg.value)); 
 }
+
+function createMsg(obj){
+
+  var bloc = document.createElement("div");
+
+  var p = document.createElement("p");
+  p.innerHTML = obj.msg;
+
+  var date = document.createElement("p");
+  date.innerHTML = obj.date + " " + obj.time;
+
+  var user = document.createElement("p");
+  user.innerHTML = obj.user; 
+
+  bloc.append(user);
+  bloc.append(p);
+  bloc.append(date);
+
+  chat.append(bloc);
+}
+
+function getMsg(all){
+
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "../htbin/chatget.py";
+  
+  xhr.responseType = "json";
+  xhr.open(method, url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = () => {
+    // In local files, status is 0 upon success in Mozilla Firefox
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      const status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        // The request has been completed successfully
+        let data = xhr.response;
+        
+        if(all){
+
+          data.forEach(element => createMsg(element));
+
+          //console.log(obj.msg);
+          console.log("connect");
+        }
+        else{
+
+          createMsg(data[data.length - 1]);
+        }
+
+      } else {
+        // Oh no! There has been an error with the request!
+        console.log("problem");
+      }
+    }
+  };
+  xhr.send(); 
+}
+
+//setInterval(getForm, 100);
+
+setTimeout(getMsg(true), 0);
